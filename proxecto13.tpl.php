f0887d806d24ad3d4b164ebb13e87cede6fe00a8
vifito <vifito@vifito.eu>
2014-09-18 01:27:00 +0200

    Funcionalidades básicas implementadas, tematizado o módulo, maquetación con bootstrap, ...

--- proxecto13.tpl.php
+++ proxecto13.tpl.php
@@ -31,7 +31,7 @@
           </table>
 
           <div>
-              <img src="<?php echo $picture ?>" class="img-thumbnail" />
+            <img src="<?php echo $picture ?>" class="img-thumbnail" />
           </div>
         </div>
         <!-- }}} -->
@@ -40,47 +40,60 @@
         <div class="col-md-6">
           <h2><?php echo t('Datos Perfil') ?></h2>
 
-          <table>
-              <tr>
-                  <th><?php echo t('Localidade') ?></th>
-                  <td><?php echo $localidade ?></td>
-              </tr>
-              <tr>
-                  <th><?php echo t('Enderezo') ?></th>
-                  <td><?php echo $enderezo ?></td>
-              </tr>
-              <tr>
-                  <th><?php echo t('Idade') ?></th>
-                  <td><?php echo $idade ?></td>
-              </tr>
-              <tr>
-                  <th><?php echo t('Teléfono Móbil') ?></th>
-                  <td><?php echo $mobil ?></td>
-              </tr>
+          <table class="table table-hover">
+            <tr>
+              <th><?php echo t('Localidade') ?></th>
+              <td><?php echo $localidade ?></td>
+            </tr>
+            <tr>
+              <th><?php echo t('Enderezo') ?></th>
+              <td><?php echo $enderezo ?></td>
+            </tr>
+            <tr>
+              <th><?php echo t('Idade') ?></th>
+              <td><?php echo $idade ?></td>
+            </tr>
+            <tr>
+              <th><?php echo t('Teléfono Móbil') ?></th>
+              <td><?php echo $mobil ?></td>
+            </tr>
           </table>
 
           <div>
-              <a class="btn btn-primary" href="<?php echo $GLOBALS['base_url'] . '/user/' . $user->uid . '/edit' ?>">
-                  <?php echo t('Modificar Perfil') ?>
-              </a>
+            <a class="btn btn-primary" href="<?php echo $base_url . '/user/' . $user->uid . '/edit' ?>">
+              <?php echo t('Modificar Perfil') ?>
+            </a>
           </div>
         </div>
         <!-- }}} -->
-
       </div>
     </div>
 
     <div class="col-md-6">
       <h2><?php echo t('Datos Entorno') ?></h2>
+
+
+      <table class="table table-hover">
+        <tr>
+          <th>Nome do sitio</th>
+          <td><?php echo $site_name ?></td>
+        </tr>
+        <tr>
+          <th>Slogan do sitio</th>
+          <td><?php echo $site_slogan ?: '<em>slogan sen definir</em>' ?></td>
+        </tr>
+      </table>
+
       <div>
-
-        <h3><?php echo $site_name ?></h3>
-
-        <h4><?php echo $site_slogan ?: 'slogan sen definir' ?></h4>
-
-        <a class="btn btn-primary" href="<?php echo $GLOBALS['base_url'] . '/proxecto13-form-contorno' ?>">Modificar Entorno</a>
-        <a class="btn btn-primary" href="<?php echo $GLOBALS['base_url'] . '/proxecto13-form-usuario' ?>">Modificar Usuario</a>
+        <a class="btn btn-primary pull-left"
+           href="<?php echo $base_url . '/proxecto13-form-contorno' ?>">Modificar
+          Entorno</a>
+        <a class="btn btn-primary pull-right"
+           href="<?php echo $base_url . '/proxecto13-form-usuario'
+           ?>">Engadir
+          Usuario</a>
       </div>
+
     </div>
   </div>
 
@@ -110,7 +123,7 @@
           <td>
             <?php echo date('d/m/Y H:i:s', $cron_last) ?>
             <a class="btn btn-primary" href="<?php echo $GLOBALS['base_url'] . '/cron.php?cron_key=' .
-  $cron_key ?>">Executa</a>
+              $cron_key ?>">Executa</a>
           </td>
         </tr>
         <tr>
@@ -119,11 +132,16 @@
         </tr>
         <tr>
           <th><?php echo t('Páxina 404') ?></th>
-          <td><?php echo $site_404 ?: 'sen definir' ?></td>
+          <td>
+            <?php echo $site_404 ? '<a href="'. $base_url . '/' . $site_404 . '">' . $site_404 . '</a>':
+                                   '<span class="text-muted">sen definir</span>' ?>
+          </td>
         </tr>
         <tr>
           <th><?php echo t('Páxina 403') ?></th>
-          <td><?php echo $site_403 ?: 'sen definir' ?></td>
+          <td>
+            <?php echo $site_403 ? '<a href="'. $base_url . '/' . $site_403 . '">' . $site_403 . '</a>':
+                                   '<span class="text-muted">sen definir</span>' ?></td>
         </tr>
       </table>
     </div>
